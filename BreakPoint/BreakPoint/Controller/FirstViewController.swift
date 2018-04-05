b4f58cbcae23e6b4819d702ee08714c9efa88698
Chetwyn <chetwynclarke@Chetwyn-Clarkes-MacBook.local>
2018-04-04 19:58:48 -0500

    First commit.
    Finished lesson 137, consisting of creating the project and setting up the MVC folder structure, along with folders for Extensions, Services and Constants.

---
+++ BreakPoint/BreakPoint/Controller/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BreakPoint
+//
+//  Created by Chetwyn on 4/4/18.
+//  Copyright © 2018 Clarke Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BreakPoint/BreakPoint/Controller/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BreakPoint
+//
+//  Created by Chetwyn on 4/4/18.
+//  Copyright © 2018 Clarke Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
