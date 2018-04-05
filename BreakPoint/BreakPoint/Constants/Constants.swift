3cd7bf948ee2ebdce5f910ea40b8961baff26669
Chetwyn <chetwynclarke@Chetwyn-Clarkes-MacBook.local>
2018-04-04 21:07:36 -0500

    Lesson 139.
    Created DataService singleton, with properties for database sections, and a function to create new users.

---
+++ BreakPoint/BreakPoint/Constants/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  BreakPoint
+//
+//  Created by Chetwyn on 4/4/18.
+//  Copyright © 2018 Clarke Enterprises. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+// MARK: - Firebase Implementation
+
+let DB_BASE = Database.database().reference()
