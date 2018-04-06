e59b94bb4aff77feb13b47571cb79765ddacee67
Chetwyn <chetwynclarke@Chetwyn-Clarkes-MacBook.local>
2018-04-05 23:40:29 -0500

    Lesson 145.
    Created MeVC along with associated set up.

---
+++ BreakPoint/BreakPoint/Controller/MeVC.swift
@@ -0,0 +1,32 @@
+//
+//  MeVC.swift
+//  BreakPoint
+//
+//  Created by Chetwyn on 4/5/18.
+//  Copyright © 2018 Clarke Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+    
+    // MARK: - Properties
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Actions
+
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+        
+        // sign out code
+    }
+    
+}
