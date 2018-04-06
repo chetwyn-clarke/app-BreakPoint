dcc7dddd81a140dd55d9c4789a1f049d64155f5c
Chetwyn <chetwynclarke@Chetwyn-Clarkes-MacBook.local>
2018-04-06 14:06:17 -0500

    Lesson 148.
    Designed FeedCell in Interface Builder, and created FeedCell file in View folder.

---
+++ BreakPoint/BreakPoint/View/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  BreakPoint
+//
+//  Created by Chetwyn on 4/6/18.
+//  Copyright © 2018 Clarke Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var contentLbl: UILabel!
+    
+    func configureCell(profileImage: UIImage, email: String, content: String) {
+        self.profileImage.image = profileImage
+        self.emailLbl.text = email
+        self.contentLbl.text = content
+    }
+
+}
